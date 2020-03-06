93d3b1d7e91f3424f7760242e15e0ed3353e09d8
D'Anne Anthony <danthony9@student.gsu.edu>
2020-03-06 13:16:04 -0500

    danne anthony

--- arithmetic3.s
+++ arithmetic3.s
@@ -1,27 +1,25 @@
-@ Natasha Mullings
-@ Arithmetic 3 Program: Register = val2 + 3 + val3 - val1
+@ danne anthony
 
 .section .data
-val1: .byte -60         @8-bit variable val1 in memory
-val2: .byte 11          @8-bit variable val2 in memory
-val3: .byte 16          @8-bit variable val3 in memory
-Register: .byte 0       @8-bit variable Register in memory
+val1: .byte -60         @8 bit variable val1 in memory
+val2: .byte 11          @8 bit variable val2 in memory
+val3: .byte 16          @8 bit variable val3 in memory
+register: .byte 0       @8 bit variable register in memory
 
 .section .text
 .globl _start
 _start:
-        ldr r0,=val1	@load the memory address of val1 into r0
-	ldrsb r0,[r0]	@load the value val1 into r0 as a signed integer
-	ldr r1,=val2	@load the memory address of val2 into r1
+        ldr r0,=val2	@load the memory address of val2 into r0
+	ldrb r0,[r0]	@load the value val2 into r0 as a unsigned integer
+	ldr r1,=val1	@load the memory address of val1 into r1
 	ldrb r1,[r1]	@load the value val2 into r1 as an unsigned integer
 	ldr r2,=val3	@load the memory address of val3 into r2
-	ldrb r2,[r2]	@load the value val3 into r2 as  an unsiged integer
-	add r1,r1,#3	@add 3 to r1 and store into r1
-	add r1,r1,r2	@add r2 to r1 and store into r1
-	sub r1,r1,r0	@subtract r0 from r1 and store into r1
-	ldrb r0,=Register @load the memory address of Register into r0
-			  @unsigned integer 
-	strb r1,[r0]	@store r0 into memory Register
+	ldrsb r2,[r2]	@load the value val3 into r2 as  an sign integer
+	add r0,r0,#3	@add 3 to r0 and store into r0
+	add r2,r2,r0	@add r0 to r2 and store into r2
+	sub r2,r2,r1	@subtract r1 from r2 and store into r2
+	ldr r3,=register @load the memory address of Register into r3
+	strb r2,[r3]	@store r2 into register
 
         mov r7,#1       @Program Termination: exit syscall
         svc #0          @Program Termination: wake kernel
