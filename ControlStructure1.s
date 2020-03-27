1d6719dcfbba72824a366b9f57ce1ddd63e2bf5a
D'Anne Anthony <danthony9@student.gsu.edu>
2020-03-26 22:11:16 -0400

    new file:   ControlStructure1 	new file:   ControlStructure1.o 	new file:   ControlStructure1.s 	new file:   barrier 	new file:   barrier.c 	new file:   fourth 	new file:   fourth.s 	new file:   masterWorker 	new file:   masterWorker.c 	new file:   trap-networking 	new file:   trap-networking.c 	new file:   trap-working

---
+++ ControlStructure1.s
@@ -0,0 +1,24 @@
+
+
+.section .data
+x: .word 3
+
+.section .text
+.global _start
+_start:
+       ldr r0, =x        @ loads the memory address of x into r0
+       ldr r0, [r0]	@ loads the value x into r0
+       cmp r0, #3        @comparing value of x with 3
+       ble truepart      @ jump if less than equal to 3
+       bgt  falsepart      @jump if greater than 3
+
+truepart: sub r1, r0, #1 @subtracting 1 and storing in x
+	  ldr r1, [r1] @ loads  the value of r1 to r1
+
+falsepart: sub r1, r0, #2 @subtracting 1 and storing in x
+	   ldr r1, [r1]   @ loads the value of r1 to r1
+       mov r7, #1        @terminating
+       svc #0
+       .end
+
+
